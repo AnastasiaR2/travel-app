d9fff1b9bb45196d9fcbdd69f849f78acd999ebe
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2024-02-18 16:26:26 +0200

    Added helper for css classes

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import App from './app.jsx';
 
-import App from "./app.jsx";
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
