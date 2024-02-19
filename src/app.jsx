43abf29085ef2522fe44ccf030a647c92b248d42
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2024-02-19 14:30:54 +0200

    Added weather forecast component

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import { WeatherForecast } from './components/components';
+
+const App = () => {
+  return <WeatherForecast />;
+};
+
+export { App };
---
+++ src/components/weather-forecast/weather-forecast.jsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+const WeatherForecast = () => {
+  return (
+    <main className={styles.container}>
+      <h1>
+        <span>Weather</span> Forecast
+      </h1>
+    </main>
+  );
+};
+
+export { WeatherForecast };
