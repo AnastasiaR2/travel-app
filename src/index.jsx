2826b89d97085b6e5071eefe80ea3c9fca966406
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2024-02-22 17:50:21 +0200

    Installed Redux Persist to save trips data to session storage

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,17 @@ import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from './app.jsx';
-import { store } from './store/store.js';
+import { persistor, store } from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
 
 import { reducer as forecastReducer } from './forecast/forecast.js';
 import { reducer as tripsReducer } from './trips/trips.js';
 
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+  whitelist: ['trips'],
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({ trips: tripsReducer, forecast: forecastReducer }),
+);
+
 const store = configureStore({
-  reducer: {
-    trips: tripsReducer,
-    forecast: forecastReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
-export { store };
+const persistor = persistStore(store);
+
+export { persistor, store };
