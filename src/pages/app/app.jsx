c480cd80d9145cc0bd06a11d920bd393eada59b6
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2024-02-22 21:30:30 +0200

    Created avatar component

--- src/pages/app/app.jsx
+++ src/pages/app/app.jsx
@@ -1,8 +1,13 @@
-import { SideInfo, WeatherForecast } from './components/components.js';
+import {
+  HeaderAvatar,
+  SideInfo,
+  WeatherForecast,
+} from './components/components.js';
 import styles from './styles.module.css';
 
 const App = () => (
   <div className={styles.wrapper}>
+    <HeaderAvatar />
     <WeatherForecast />
     <SideInfo />
   </div>
--- src/pages/app/components/components.js
+++ src/pages/app/components/components.js
@@ -1,2 +1,3 @@
+export { HeaderAvatar } from './header-avatar/header-avatar.jsx';
 export { SideInfo } from './side-info/side-info.jsx';
 export { WeatherForecast } from './weather-forecast/weather-forecast.jsx';
